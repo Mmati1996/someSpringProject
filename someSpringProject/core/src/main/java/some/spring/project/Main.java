e38a119808aa270dd894c591f16d0276dbb44f95
Mateusz <mat.duk.at@gmail.com>
2023-01-06 13:51:42 +0100

    basic setup

---
+++ someSpringProject/core/src/main/java/some/spring/project/Main.java
@@ -0,0 +1,12 @@
+package some.spring.project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("just logging to check if everything works");
+    }
+}
